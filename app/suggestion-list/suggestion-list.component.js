7888bfe326e8f67f921d51557f8921f99a226f04
Nick Edwards <BungleJook@users.noreply.github.com>
2016-07-12 19:46:58 +0100

    Add addSuggestion function for suggestion-list component; update suggestions.json format

--- app/suggestion-list/suggestion-list.component.js
+++ app/suggestion-list/suggestion-list.component.js
@@ -7,6 +7,21 @@ angular
                 var self = this;
 
                 self.suggestions = Suggestion.query();
+                
+                self.addSuggestion = function() {
+                    // if input empty, don't submit
+                    if (!self.title || self.title === "") {
+                        return;
+                    }
+                    // push suggestions
+                    self.suggestions.push({
+                        title: self.title,
+                        upvotes: 0,
+                        date: new Date().toISOString()
+                    })
+                    // clear input
+                    self.title = '';
+                }
             }
         ]
     });
